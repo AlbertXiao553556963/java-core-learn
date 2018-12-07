3b0f5b2fcc375c35c15b4656297a45777fca3118
肖铭轩 <xmx553556963@gmail.com>
2018-12-07 09:58:42 +0800

    add stream

--- canal-learn/src/main/java/com/xmx/canal/SimpleCanalClientExample.java
+++ canal-learn/src/main/java/com/xmx/canal/SimpleCanalClientExample.java
@@ -14,8 +14,10 @@ import com.alibaba.otter.canal.protocol.CanalEntry.EntryType;
 import com.alibaba.otter.canal.protocol.CanalEntry.EventType;
 import com.alibaba.otter.canal.protocol.CanalEntry.RowChange;
 import com.alibaba.otter.canal.protocol.CanalEntry.RowData;
+import com.google.common.base.Throwables;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 public class SimpleCanalClientExample {
 
 
@@ -24,25 +26,22 @@ public class SimpleCanalClientExample {
         CanalConnector connector = CanalConnectors.newSingleConnector(new InetSocketAddress(AddressUtils.getHostIp(),
                 11111), "example", "", "");
         int batchSize = 1000;
-        int emptyCount = 0;
         try {
             connector.connect();
             connector.subscribe(".*\\..*");
             connector.rollback();
-            int totalEmptyCount = 120;
-            while (emptyCount < totalEmptyCount) {
+            while (true) {
                 Message message = connector.getWithoutAck(batchSize); // 获取指定数量的数据
                 long batchId = message.getId();
                 int size = message.getEntries().size();
+
+                // 如果是空数据，客户端休眠1s
                 if (batchId == -1 || size == 0) {
-                    emptyCount++;
-                    System.out.println("empty count : " + emptyCount);
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                     }
                 } else {
-                    emptyCount = 0;
                     printEntry(message.getEntries());
                 }
 
@@ -50,8 +49,10 @@ public class SimpleCanalClientExample {
                 // connector.rollback(batchId); // 处理失败, 回滚数据
             }
 
-            System.out.println("empty too many times, exit");
-        } finally {
+        } catch (Exception e) {
+            log.error(Throwables.getStackTraceAsString(e));
+        }
+        finally {
             connector.disconnect();
         }
     }
---
+++ stream-learn/src/main/java/com/TestReduce.java
@@ -0,0 +1,38 @@
+package com;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author: XiaoMingxuan
+ * @email: mingxuan.xmx@alibaba-inc.com
+ * @create: 2018-12-06 19:46
+ **/
+public class TestReduce {
+
+    @Data
+    static class Account {
+        BigDecimal ammount;
+    }
+
+    public static void main(String[] args) {
+        Account account1 = new Account();
+        Account account2 = new Account();
+        Account account3 = new Account();
+        account1.setAmmount(new BigDecimal(10));
+        account2.setAmmount(new BigDecimal(20));
+        account3.setAmmount(new BigDecimal(30));
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(account1);
+        accounts.add(account2);
+        accounts.add(account3);
+
+        Optional<BigDecimal> remain = accounts.stream().map(Account::getAmmount).reduce(BigDecimal::add);
+
+        System.out.println(remain);
+    }
+}
