a7d39d5bbcf25f9ba675b51900ae2f3c6acc8a68
肖铭轩 <xmx553556963@gmail.com>
2019-01-11 19:04:31 +0800

    基于park/unpark和cas机制的可重入锁

---
+++ wangyi-micro-java/src/main/java/topic1/lock/MyReentrantLockV2.java
@@ -0,0 +1,67 @@
+package topic1.lock;
+
+import java.util.Iterator;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author: XiaoMingxuan
+ * @email: mingxuan.xmx@alibaba-inc.com
+ * @create: 2019-01-11 18:44
+ **/
+public class MyReentrantLockV2 implements Lock {
+
+    private AtomicReference<Thread> owner = new AtomicReference<>();
+
+    private LinkedBlockingQueue<Thread> waiters = new LinkedBlockingQueue<>();
+
+    @Override
+    public boolean tryLock() {
+        return owner.compareAndSet(null, Thread.currentThread());
+    }
+
+    @Override
+    public void lock() {
+        boolean isAdd = true;
+        while (!tryLock()) {
+            if(isAdd) {
+                waiters.add(Thread.currentThread());
+                isAdd = false;
+            }
+            LockSupport.park();
+        }
+        waiters.remove(Thread.currentThread());
+    }
+
+    @Override
+    public void unlock() {
+        if(owner.compareAndSet(Thread.currentThread(), null)) {
+            Iterator<Thread> iterator = waiters.iterator();
+            while (iterator.hasNext()) {
+                Thread waiter = iterator.next();
+                LockSupport.unpark(waiter);
+            }
+        }
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
