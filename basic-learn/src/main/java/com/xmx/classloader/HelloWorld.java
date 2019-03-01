bec76b5b8f0105c65ed03a55dbe768b6ba37bebe
xiaomingxuan <xiaomingxuan@bbktel.com>
2019-03-01 18:12:17 +0800

    feat: 实现一个扫描类文件变动，自动加载类

---
+++ basic-learn/src/main/java/com/xmx/classloader/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.xmx.classloader;
+
+/**
+ * @Auther: 11092893 xiaomingxuan
+ * @Date: 2019/3/1 15:08
+ * @Description:
+ */
+public class HelloWorld {
+
+    public void say() {
+        System.out.println("hello world 1");
+    }
+
+}
